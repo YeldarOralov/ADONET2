45be23f9bc3421e78ea141439eb3d10a93c7ded3
Eldar Oralov <eldar.oralov@gmail.com>
2019-04-12 00:59:33 +0600

    Добавлены файлы проекта

---
+++ HomeWork2/HomeWork2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["appContext"].ConnectionString))
+            {
+                connection.Open();
+                string commandText = "create table [Group] (id int NOT NULL Primary key Identity(1,1), Name nvarchar(MAX) NOT NULL)";
+                var command = new SqlCommand(commandText, connection);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
